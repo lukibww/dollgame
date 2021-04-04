07291b886e8e625f7b20a50482b8bb4c4037247f
lukibw <lukasz.b.wlodarczyk@gmail.com>
2021-04-04 10:07:50 +0200

    refactor(Database): Design better db api

---
+++ src/components/database/database-context.ts
@@ -0,0 +1,48 @@
+import { createContext } from "react";
+
+export type DatabaseRecord = Record<string, any>;
+
+export interface DatabaseIndex<T extends DatabaseRecord> {
+  keyPath: string | string[];
+  multiEntry: boolean;
+  name: string;
+  objectStore: DatabaseStore<T>;
+  unique: boolean;
+  get: (key: string) => Promise<T>;
+}
+
+export interface DatabaseStore<T extends DatabaseRecord> {
+  exists: (key: string) => Promise<boolean>;
+  get: (key: string) => Promise<T>;
+  update: (key: string, record: Partial<T>) => Promise<void>;
+  insert: (record: T) => Promise<void>;
+  delete: (key: string) => Promise<void>;
+  cursor: (callback: (record: T, next: () => void) => void) => Promise<void>;
+  clear: () => Promise<void>;
+  createIndex: (
+    name: string,
+    key: string,
+    options?: IDBIndexParameters
+  ) => Promise<DatabaseIndex<T>>;
+  getIndex: (name: string) => Promise<DatabaseIndex<T>>;
+  deleteIndex: (name: string) => Promise<void>;
+}
+
+export interface Database {
+  createStore: <T extends DatabaseRecord>(
+    name: string
+  ) => Promise<DatabaseStore<T>>;
+  getStore: <T extends DatabaseRecord>(
+    name: string
+  ) => Promise<DatabaseStore<T>>;
+}
+
+export interface DatabaseContextValue {
+  database: Database | null;
+  error: string | null;
+  loading: boolean;
+}
+
+const DatabaseContext = createContext<DatabaseContextValue | null>(null);
+
+export default DatabaseContext;
---
+++ src/components/database/database.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export interface DatabaseProps {
+  children?: ReactNode;
+}
+
+function Database({ children }: DatabaseProps) {}
+
+export default Database;
